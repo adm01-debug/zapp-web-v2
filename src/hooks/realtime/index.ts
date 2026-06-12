a910e26304e0b471ac61ba05f680d0c90293a641
adm01-debug <adm01@promobrindes.com.br>
2026-06-12 08:39:04 -0300

    feat(hooks): barrel export for hooks/realtime (5 exports)

---
+++ src/hooks/realtime/index.ts
@@ -0,0 +1,6 @@
+// Barrel export for hooks/realtime
+export * from './realtimeUtils';
+export * from './messageSender';
+export * from './useMessageUpdateBatcher';
+export * from './useRealtimeNotifications';
+export * from './useSupabaseRealtime';
