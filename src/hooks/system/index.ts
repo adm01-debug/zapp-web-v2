3b2540bad26daff05ae8e4e9720da10880b16ac1
adm01-debug <adm01@promobrindes.com.br>
2026-06-12 08:30:39 -0300

    feat(hooks): add barrel export for hooks/system
    Enables: import { useDebounce, usePermissions, useGlobalSettings } from '@/hooks/system'
    instead of 33 separate deep-path imports.

---
+++ src/hooks/system/index.ts
@@ -0,0 +1,36 @@
+// Barrel export for hooks/system
+// Import from this file instead of individual modules:
+//   import { useDebounce, usePermissions, useGlobalSettings } from '@/hooks/system'
+export * from './useCRUD';
+export * from './useCurrentModule';
+export * from './useDebounce';
+export * from './useDiagnosticsData';
+export * from './useDownloadPermission';
+export * from './useDuplicate';
+export * from './useExportData';
+export * from './useGeoBlocking';
+export * from './useGlobalSettings';
+export * from './useIdleCallback';
+export * from './useImportData';
+export * from './useInfiniteScroll';
+export * from './useNavigationHistory';
+export * from './useNetworkStatus';
+export * from './useNotificationSettings';
+export * from './useNotifications';
+export * from './useOfflineCache';
+export * from './usePermissions';
+export * from './usePrefetch';
+export * from './usePushNotifications';
+export * from './useRateLimitLogs';
+export * from './useResourcePrefetch';
+export * from './useSearch';
+export * from './useSearchHistory';
+export * from './useSecurityPushNotifications';
+export * from './useServiceWorker';
+export * from './useSkillBasedAssign';
+export * from './useSupabaseMutation';
+export * from './useUndoableAction';
+export * from './useUrlFilters';
+export * from './useUserRole';
+export * from './useUserSettings';
+export * from './useVersions';
