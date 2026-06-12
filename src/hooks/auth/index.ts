e3624feb731365932a955ed3ecb99d003cbceeb7
adm01-debug <adm01@promobrindes.com.br>
2026-06-12 08:30:20 -0300

    feat(hooks): add barrel export for hooks/auth
    Enables: import { useAuth, useMFA, useWebAuthn } from '@/hooks/auth'
    instead of 6 separate deep-path imports.

---
+++ src/hooks/auth/index.ts
@@ -0,0 +1,9 @@
+// Barrel export for hooks/auth
+// Import from this file instead of individual modules:
+//   import { useAuth, useMFA } from '@/hooks/auth'
+export * from './useAuth';
+export * from './useAuthForm';
+export * from './useMFA';
+export * from './useReauthentication';
+export * from './useSecureProfile';
+export * from './useWebAuthn';
