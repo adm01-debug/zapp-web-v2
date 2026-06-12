ee4534ade1c7f083abbd5039328c497daa489f5e
adm01-debug <adm01@promobrindes.com.br>
2026-06-12 08:40:23 -0300

    feat(hooks): barrel export for hooks/business (13 hooks)

---
+++ src/hooks/business/index.ts
@@ -0,0 +1,14 @@
+// Barrel export for hooks/business (13 hooks)
+export * from './useBusinessHours';
+export * from './useBusinessHoursCheck';
+export * from './useCSAT';
+export * from './useClientWallet';
+export * from './useDemandPrediction';
+export * from './useNPSSurveys';
+export * from './useQueueAnalytics';
+export * from './useQueueGoals';
+export * from './useQueues';
+export * from './useQueuesComparison';
+export * from './useShoppingCart';
+export * from './useWarRoomAlerts';
+export * from './useWarRoomData';
