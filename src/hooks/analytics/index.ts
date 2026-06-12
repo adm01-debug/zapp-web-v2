b5bfbdfb80c7c08860d4b5e59c23acca27b0a8c6
adm01-debug <adm01@promobrindes.com.br>
2026-06-12 08:36:39 -0300

    feat(hooks): barrel export for hooks/analytics (10 hooks)

---
+++ src/hooks/analytics/index.ts
@@ -0,0 +1,11 @@
+// Barrel export for hooks/analytics
+export * from './useAIStats';
+export * from './useAIUsageDashboard';
+export * from './useDashboardData';
+export * from './useDashboardWidgets';
+export * from './useGoalNotifications';
+export * from './useGoalsDashboard';
+export * from './usePerformance';
+export * from './usePerformanceOptimizations';
+export * from './usePerformanceSnapshots';
+export * from './useRealtimeDashboard';
