1274ab1aaa196c736f8e4ead02c8f8264fb71397
adm01-debug <adm01@promobrindes.com.br>
2026-06-12 08:30:57 -0300

    feat(hooks): add barrel export for hooks/crm (17 hooks)

---
+++ src/hooks/crm/index.ts
@@ -0,0 +1,20 @@
+// Barrel export for hooks/crm
+// Import from this file instead of individual modules:
+//   import { useAgents, useTags, useContactNotes } from '@/hooks/crm'
+export * from './useAdvancedContactSearch';
+export * from './useAgentReassignment';
+export * from './useAgents';
+export * from './useContactAssignment';
+export * from './useContactCustomFields';
+export * from './useContactEnrichedData';
+export * from './useContactIntelligence';
+export * from './useContactNotes';
+export * from './useContactStats';
+export * from './useContactsSearch';
+export * from './useExternalCargos';
+export * from './useExternalContact360';
+export * from './useExternalContact360Batch';
+export * from './useExternalEmpresas';
+export * from './useTags';
+export * from './useTeamProfiles';
+export * from './useVisibleAgents';
