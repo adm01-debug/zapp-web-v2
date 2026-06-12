3f708d22f56ab695d1ba163d1753ffab4f03e0bc
adm01-debug <adm01@promobrindes.com.br>
2026-06-12 08:37:27 -0300

    feat(hooks): barrel export for hooks/communication (11 hooks)

---
+++ src/hooks/communication/index.ts
@@ -0,0 +1,12 @@
+// Barrel export for hooks/communication
+export * from './useAudioMemes';
+export * from './useAudioPlayer';
+export * from './useAudioRecorder';
+export * from './useCalls';
+export * from './useCampaigns';
+export * from './useIncomingCallListener';
+export * from './useSipClient';
+export * from './useSpeechToText';
+export * from './useTextToSpeech';
+export * from './useTranscriptionNotifications';
+export * from './useVoiceAgent';
