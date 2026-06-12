b5e0f1884e26801f41555c50b40426ad70a05bbe
adm01-debug <adm01@promobrindes.com.br>
2026-06-12 08:31:19 -0300

    feat(hooks): add barrel export for hooks/ui (30 hooks)
    Enables: import { useTheme, useKeyboardShortcuts, useMobile } from '@/hooks/ui'
    instead of 30 separate deep-path imports.

---
+++ src/hooks/ui/index.ts
@@ -0,0 +1,33 @@
+// Barrel export for hooks/ui
+// Import from this file instead of individual modules:
+//   import { useTheme, useKeyboardShortcuts, useDeviceDetection } from '@/hooks/ui'
+export * from './use-mobile';
+export * from './use-toast';
+export * from './useActionFeedback';
+export * from './useAmbientColor';
+export * from './useAriaAnnouncer';
+export * from './useCustomShortcuts';
+export * from './useDeepLinks';
+export * from './useDensity';
+export * from './useDeviceDetection';
+export * from './useDocumentTitle';
+export * from './useGlobalKeyboardShortcuts';
+export * from './useGlobalSearchShortcut';
+export * from './useKeyboardHeight';
+export * from './useKeyboardNavigation';
+export * from './useKeyboardShortcuts';
+export * from './useLoadingState';
+export * from './useOnboarding';
+export * from './useOnboardingChecklist';
+export * from './useParallax';
+export * from './usePrefetchOnHover';
+export * from './usePullToRefresh';
+export * from './useScreenProtection';
+export * from './useSidebarCollapse';
+export * from './useSidebarFavorites';
+export * from './useSwipeGesture';
+export * from './useSwipeNavigation';
+export * from './useTheme';
+export * from './useUniversityHelp';
+export * from './useViewTransition';
+export * from './useZenMode';
