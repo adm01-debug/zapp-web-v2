0af1e77f3ff3db880a05ac36b6a722163fb99212
adm01-debug <adm01@promobrindes.com.br>
2026-06-12 08:37:56 -0300

    feat(hooks): barrel export for hooks/gamification (5 exports: types, utils + hooks)

---
+++ src/hooks/gamification/index.ts
@@ -0,0 +1,6 @@
+// Barrel export for hooks/gamification
+export * from './types';
+export * from './levelUtils';
+export * from './mutations';
+export * from './useAgentGamification';
+export * from './useLeaderboard';
