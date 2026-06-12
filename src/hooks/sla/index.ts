efacad9e48043f13134115fffdc91e877c36bba7
adm01-debug <adm01@promobrindes.com.br>
2026-06-12 08:39:36 -0300

    feat(hooks): barrel export for hooks/sla (7 hooks)

---
+++ src/hooks/sla/index.ts
@@ -0,0 +1,8 @@
+// Barrel export for hooks/sla
+export * from './useApplicableSLA';
+export * from './useSLACalculation';
+export * from './useSLAConfigurations';
+export * from './useSLAHistory';
+export * from './useSLAMetrics';
+export * from './useSLANotifications';
+export * from './useSLARules';
