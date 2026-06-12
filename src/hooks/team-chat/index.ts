9b8e6c3eb5b03794b83013e79b3fe6da30812624
adm01-debug <adm01@promobrindes.com.br>
2026-06-12 08:40:04 -0300

    feat(hooks): barrel export for hooks/team-chat (4 exports)

---
+++ src/hooks/team-chat/index.ts
@@ -0,0 +1,5 @@
+// Barrel export for hooks/team-chat
+export * from './teamChatTypes';
+export * from './useTeamChatMutations';
+export * from './useTeamConversations';
+export * from './useTeamMessages';
