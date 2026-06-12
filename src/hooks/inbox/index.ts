1b85d5a7c889c59b0ac0b094dbc9374e99c4f5c3
adm01-debug <adm01@promobrindes.com.br>
2026-06-12 08:30:28 -0300

    feat(hooks): add barrel export for hooks/inbox
    Enables: import { useRealtimeInbox, useBulkActions } from '@/hooks/inbox'
    instead of 11 separate deep-path imports.

---
+++ src/hooks/inbox/index.ts
@@ -0,0 +1,14 @@
+// Barrel export for hooks/inbox
+// Import from this file instead of individual modules:
+//   import { useRealtimeInbox, useBulkActions, useInboxFilters } from '@/hooks/inbox'
+export * from './useAutoCloseConversations';
+export * from './useBulkActions';
+export * from './useConnectionQueues';
+export * from './useConnectionsManager';
+export * from './useInboxBulkActions';
+export * from './useInboxFilters';
+export * from './useInboxUIState';
+export * from './useObjectionDetector';
+export * from './useRealtimeInbox';
+export * from './useRealtimeSentimentAlerts';
+export * from './useSentimentAlerts';
