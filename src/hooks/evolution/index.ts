0ce3f21b29f1897aace4eb4455aeff0c2c3907e8
adm01-debug <adm01@promobrindes.com.br>
2026-06-12 08:37:13 -0300

    feat(hooks): barrel export for hooks/evolution (5 hooks)

---
+++ src/hooks/evolution/index.ts
@@ -0,0 +1,6 @@
+// Barrel export for hooks/evolution
+export * from './useEvolutionApiCore';
+export * from './useEvolutionGroups';
+export * from './useEvolutionInstance';
+export * from './useEvolutionIntegrations';
+export * from './useEvolutionMessaging';
