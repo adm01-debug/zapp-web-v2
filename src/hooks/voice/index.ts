404da292397679b639d1bf267a5b61a233e7591e
adm01-debug <adm01@promobrindes.com.br>
2026-06-12 08:40:10 -0300

    feat(hooks): barrel export for hooks/voice (6 exports: types + utilities + hook)

---
+++ src/hooks/voice/index.ts
@@ -0,0 +1,7 @@
+// Barrel export for hooks/voice
+export * from './types';
+export * from './retry';
+export * from './logVoiceCommand';
+export * from './playTtsAudio';
+export * from './processTranscript';
+export * from './useVoiceAgent';
